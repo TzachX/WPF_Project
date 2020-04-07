99db0ecc7a8146e2bcdeb3f4893abc579b843bf0
Tzach <TzachX@gmail.com>
2020-04-07 04:59:12 +0300

    Initial Upload
    Includes:
    View (XAML + Code Behind) for the Joystick
    View (Just XAML) for the Dashboard
    A basic skeleton of the TCP Client

---
+++ Models/TelnetClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1.Models
+{
+    class TelnetClient
+    {
+        private Socket socket;
+        private NetworkStream networkStream;
+        private TcpClient client;
+
+        private void Connect(String ip, String port)
+        {
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket.Connect(ip, Int32.Parse(port));
+                Console.WriteLine("Connected"); //Test purpose only
+            }
+            catch
+            {
+                Console.WriteLine("ERROR"); //Test purpose only
+            }
+        }
+
+        private void readInfo()
+        {
+            string command, recieved;
+
+            //Read first thing givent o us
+            recieved = read();
+            Console.WriteLine(recieved);
+
+            //Set up connection loop
+            while (true)
+            {
+                Console.Write("Response: ");
+                command = Console.ReadLine();
+
+                if (command == "exit")
+                    break;
+
+                //write(command);
+                recieved = read();
+
+                Console.WriteLine(recieved);
+            }
+
+            Console.WriteLine("Disconnected from server");
+            networkStream.Close();
+            client.Close();
+        }
+
+        public string read()
+        {
+            byte[] data = new byte[1024];
+            string recieved = "";
+
+            int size = networkStream.Read(data, 0, data.Length);
+            recieved = Encoding.ASCII.GetString(data, 0, size);
+
+            return recieved;
+        }
+    }
+}
---
+++ Views/Joystick.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FlightSimulator.Views
+{
+    /// <summary>
+    /// Interaction logic for Joystick.xaml
+    /// </summary>
+    public partial class Joystick : UserControl
+    {
+        private Point LastPos = new Point();
+        private readonly Storyboard centerKnob;
+        public static readonly DependencyProperty elevator = DependencyProperty.Register("Elevator", typeof(double), typeof(Joystick), null);
+        public static readonly DependencyProperty rudder = DependencyProperty.Register("Rudder", typeof(double), typeof(Joystick), null);
+        public static readonly DependencyProperty aileronUnit = DependencyProperty.Register("AileronUnit", typeof(double), typeof(Joystick), null);
+        //public static readonly DependencyProperty elevatorUnit = DependencyProperty.Register("elevatorUnit", typeof(double), typeof(Joystick), null);
+
+        public double Rudder
+        {
+            get { return Convert.ToDouble(GetValue(rudder)); }
+            set { SetValue(rudder, value); }
+        }
+
+        public double AileronUnit
+        {
+            get { return Convert.ToDouble(GetValue(aileronUnit)); }
+            set
+            {
+                if (value < 1) value = 1; else if (value > 90) value = 90;
+                SetValue(aileronUnit, Math.Round(value));
+            }
+        }
+
+        public double Elevator
+        {
+            get { return Convert.ToDouble(GetValue(elevator)); }
+            set { SetValue(elevator, value); }
+        }
+
+        //public double ElevatorUnit
+        //{
+        //    get { return Convert.ToDouble(GetValue(elevatorUnit)); }
+        //    set
+        //    {
+        //        if (value < 1) value = 1; else if (value > 90) value = 90;
+        //        SetValue(elevatorUnit, Math.Round(value));
+        //    }
+        //}
+
+        public Joystick()
+        {
+            InitializeComponent();
+            centerKnob = Knob.Resources["CenterKnob"] as Storyboard;
+        }
+        private void centerKnob_Completed(object o, EventArgs e) 
+        {
+            knobPosition.X = Rudder = 0;
+            knobPosition.Y = Elevator = 0;
+            Storyboard animStory = (Storyboard)Knob.Resources["CenterKnob"];
+           centerKnob.Stop();
+        }
+
+        private void Knob_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                double xVal = e.GetPosition(this).X - LastPos.X;
+                double yVal = e.GetPosition(this).Y - LastPos.Y;
+
+                // If we "got out" of the circle, change movement to border movement
+                if (Math.Sqrt(Math.Pow(xVal, 2) + Math.Pow(yVal, 2)) > Base.Width / 4)
+                {
+                    double angle = Math.Atan2(0 - yVal, xVal - 0);
+                    xVal = Math.Cos(angle) * (Base.Width / 4);
+                    yVal = Math.Sin(-angle) * (Base.Width / 4);
+                    
+                }
+
+                // Update values
+                knobPosition.X = xVal;
+                Rudder = xVal / 85;
+                Rudder = Convert.ToDouble(String.Format("{0:0.0}", Rudder));
+                knobPosition.Y = yVal;
+                Elevator = yVal / 85;
+                Elevator = Convert.ToDouble(String.Format("{0:0.0}", -Elevator));
+            }
+        }
+
+        private void Knob_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            centerKnob.Begin();
+            Knob.ReleaseMouseCapture();
+        }
+
+        private void Knob_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            LastPos = e.GetPosition(this);
+            Knob.CaptureMouse();
+        }
+    }
+}
