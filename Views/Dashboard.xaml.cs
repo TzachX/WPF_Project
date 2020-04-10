e7a7f09e43faed056b9ba21410b7a6d57cb86208
Tzach <TzachX@gmail.com>
2020-04-10 05:18:02 +0300

    GUI version before server testing
    Added:
    • Map
    • Comments for the Joystick and Dashboard xaml.cs files
    • Binding for the dashboard

--- Views/Dashboard.xaml.cs
+++ Views/Dashboard.xaml.cs
@@ -20,6 +20,73 @@ namespace Ex1.Views
     /// </summary>
     public partial class Dashboard : UserControl
     {
+        // Properties definition
+        public static readonly DependencyProperty headingDeg = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+        public static readonly DependencyProperty verSpeed = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+        public static readonly DependencyProperty groundSpeed = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+        public static readonly DependencyProperty speed = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+        public static readonly DependencyProperty altitude = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+        public static readonly DependencyProperty rollDeg = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+        public static readonly DependencyProperty pitchDeg = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+        public static readonly DependencyProperty altimAltitude = DependencyProperty.Register("Elevator", typeof(double), typeof(Dashboard), null);
+
+        // Getter + Setter for the headingDeg dependency property
+        public double HeadingDegree
+        {
+            get { return Convert.ToDouble(GetValue(headingDeg)); }
+            set { SetValue(headingDeg, value); }
+        }
+
+        // Getter + Setter for the verSpeed dependency property
+        public double VerticalSpeed
+        {
+            get { return Convert.ToDouble(GetValue(verSpeed)); }
+            set { SetValue(verSpeed, value); }
+        }
+
+        // Getter + Setter for the groundSpeed dependency property
+        public double GroundSpeed
+        {
+            get { return Convert.ToDouble(GetValue(groundSpeed)); }
+            set { SetValue(groundSpeed, value); }
+        }
+
+        // Getter + Setter for the speed dependency property
+        public double Speed
+        {
+            get { return Convert.ToDouble(GetValue(speed)); }
+            set { SetValue(speed, value); }
+        }
+
+        // Getter + Setter for the altitude dependency property
+        public double Altitude
+        {
+            get { return Convert.ToDouble(GetValue(altitude)); }
+            set { SetValue(altitude, value); }
+        }
+
+        // Getter + Setter for the rollDeg dependency property
+        public double RollDegree
+        {
+            get { return Convert.ToDouble(GetValue(rollDeg)); }
+            set { SetValue(rollDeg, value); }
+        }
+
+        // Getter + Setter for the pitchDeg dependency property
+        public double PitchDegree
+        {
+            get { return Convert.ToDouble(GetValue(pitchDeg)); }
+            set { SetValue(pitchDeg, value); }
+        }
+
+        // Getter + Setter for the altimAltitude dependency property
+        public double AltimAltitude
+        {
+            get { return Convert.ToDouble(GetValue(altimAltitude)); }
+            set { SetValue(altimAltitude, value); }
+        }
+
+        // C-tor
         public Dashboard()
         {
             InitializeComponent();
--- Views/Joystick.xaml.cs
+++ Views/Joystick.xaml.cs
@@ -20,68 +20,50 @@ namespace FlightSimulator.Views
     /// </summary>
     public partial class Joystick : UserControl
     {
+        // Properties definition
         private Point LastPos = new Point();
         private readonly Storyboard centerKnob;
         public static readonly DependencyProperty elevator = DependencyProperty.Register("Elevator", typeof(double), typeof(Joystick), null);
         public static readonly DependencyProperty rudder = DependencyProperty.Register("Rudder", typeof(double), typeof(Joystick), null);
-        public static readonly DependencyProperty aileronUnit = DependencyProperty.Register("AileronUnit", typeof(double), typeof(Joystick), null);
-        //public static readonly DependencyProperty elevatorUnit = DependencyProperty.Register("elevatorUnit", typeof(double), typeof(Joystick), null);
+        const int MAX_JOYSTICK_VALUE = 85;
 
+        // Getter + Setter for the rudder dependency property
         public double Rudder
         {
             get { return Convert.ToDouble(GetValue(rudder)); }
             set { SetValue(rudder, value); }
         }
 
-        public double AileronUnit
-        {
-            get { return Convert.ToDouble(GetValue(aileronUnit)); }
-            set
-            {
-                if (value < 1) value = 1; else if (value > 90) value = 90;
-                SetValue(aileronUnit, Math.Round(value));
-            }
-        }
-
+        // Getter + Setter for the elevator dependency property
         public double Elevator
         {
             get { return Convert.ToDouble(GetValue(elevator)); }
             set { SetValue(elevator, value); }
         }
-
-        //public double ElevatorUnit
-        //{
-        //    get { return Convert.ToDouble(GetValue(elevatorUnit)); }
-        //    set
-        //    {
-        //        if (value < 1) value = 1; else if (value > 90) value = 90;
-        //        SetValue(elevatorUnit, Math.Round(value));
-        //    }
-        //}
-
-        public Joystick()
-        {
-            InitializeComponent();
-            centerKnob = Knob.Resources["CenterKnob"] as Storyboard;
-        }
+        
+        // Reset the actual knob position when the animation ends
         private void centerKnob_Completed(object o, EventArgs e) 
         {
             knobPosition.X = Rudder = 0;
             knobPosition.Y = Elevator = 0;
             Storyboard animStory = (Storyboard)Knob.Resources["CenterKnob"];
-           centerKnob.Stop();
+            centerKnob.Stop();
         }
 
+        // Call this function if the mouse is moved
         private void Knob_MouseMove(object sender, MouseEventArgs e)
         {
+            // If the mouse is moved AND the left mouse button is pressed
             if (e.LeftButton == MouseButtonState.Pressed)
             {
+                // Initialize the x and y values of the cursor
                 double xVal = e.GetPosition(this).X - LastPos.X;
                 double yVal = e.GetPosition(this).Y - LastPos.Y;
 
-                // If we "got out" of the circle, change movement to border movement
+                // If we "got out" of the circle, change movement to a cirucular border movement
                 if (Math.Sqrt(Math.Pow(xVal, 2) + Math.Pow(yVal, 2)) > Base.Width / 4)
                 {
+                    // Calculate the position according to the angle created between the cursor and the center of the joystick
                     double angle = Math.Atan2(0 - yVal, xVal - 0);
                     xVal = Math.Cos(angle) * (Base.Width / 4);
                     yVal = Math.Sin(-angle) * (Base.Width / 4);
@@ -90,24 +72,34 @@ namespace FlightSimulator.Views
 
                 // Update values
                 knobPosition.X = xVal;
-                Rudder = xVal / 85;
-                Rudder = Convert.ToDouble(String.Format("{0:0.0}", Rudder));
+                Rudder = xVal / MAX_JOYSTICK_VALUE;
+                Rudder = Convert.ToDouble(String.Format("{0:0.0}", Rudder)); // For nicer display and UX
                 knobPosition.Y = yVal;
-                Elevator = yVal / 85;
-                Elevator = Convert.ToDouble(String.Format("{0:0.0}", -Elevator));
+                Elevator = yVal / MAX_JOYSTICK_VALUE;
+                Elevator = Convert.ToDouble(String.Format("{0:0.0}", -Elevator)); // For nicer display and UX
             }
         }
 
+        // Call this function if the mouse's button is released
         private void Knob_MouseUp(object sender, MouseButtonEventArgs e)
         {
+            // Begin the animation and return the knob to its place
             centerKnob.Begin();
             Knob.ReleaseMouseCapture();
         }
 
+        // Call this function if the mouse's left button is pressed
         private void Knob_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             LastPos = e.GetPosition(this);
             Knob.CaptureMouse();
+        }
+
+        // C-tor
+        public Joystick()
+        {
+            InitializeComponent();
+            centerKnob = Knob.Resources["CenterKnob"] as Storyboard;
         }
     }
 }
