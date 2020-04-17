cec7ac2b6052b6cd73d2761a5729956b9439d202
Tzach <TzachX@gmail.com>
2020-04-17 16:21:11 +0300

    MapViewModel and some tweaks
    Changed the flightcontrols (joystick and sliders) view so it won't contain any dependencyproperties (not according to mvvm principles)

---
+++ ViewModels/MapViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Ex1.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1.ViewModels
+{
+    class MapViewModel
+    {
+        private IPlaneModel planeModel;
+        public MapViewModel(IPlaneModel model)
+        {
+            this.planeModel = model;
+            model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyProperyChange("VM_" + e.PropertyName);
+            };
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyProperyChange(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public double VM_Location { get { return planeModel.AirSpeed; } }
+        public double VM_Longtitude { get { return planeModel.PlaneVert; } }
+        public double VM_Latitude { get { return planeModel.PlaneHoriz; } }
+    }
+}
--- Views/FlightControls.xaml.cs
+++ Views/FlightControls.xaml.cs
@@ -27,49 +27,12 @@ namespace Ex1.Views
     public partial class FlightControls : UserControl
     {
         JoystickViewModel joystickVM;
-        public static readonly DependencyProperty fcRudder = DependencyProperty.Register("FC_Rudder", typeof(double), typeof(FlightControls), null);
-        public static readonly DependencyProperty fcElevator = DependencyProperty.Register("FC_Elevator", typeof(double), typeof(FlightControls), null);
-        public static readonly DependencyProperty fcThrottle = DependencyProperty.Register("FC_Throttle", typeof(double), typeof(FlightControls), null);
-        public static readonly DependencyProperty fcAileron = DependencyProperty.Register("FC_Aileron", typeof(double), typeof(FlightControls), null);
-
-        // Getter + Setter for the rudder dependency property
-        public double FC_Rudder
-        {
-            get { return Convert.ToDouble(GetValue(fcRudder)); }
-            set { SetValue(fcRudder, value); /*joystickVM.VM_Rudder = value;*/ }
-        }
-
-        // Getter + Setter for the elevator dependency property
-        public double FC_Elevator
-        {
-            get { return Convert.ToDouble(GetValue(fcElevator)); }
-            set { SetValue(fcElevator, value); /*joystickVM.VM_Elevator = value;*/ }
-        }
-
-        // Getter + Setter for the throttle dependency property
-        public double FC_Throttle
-        {
-            get { return Convert.ToDouble(GetValue(fcThrottle)); }
-            set { SetValue(fcThrottle, value); /*joystickVM.VM_Throttle = value;*/
-            }
-        }
-
-        // Getter + Setter for the aileron dependency property
-        public double FC_Aileron
-        {
-            get { return Convert.ToDouble(GetValue(fcAileron)); }
-            set { SetValue(fcAileron, value); /*joystickVM.VM_Aileron = value;*/ }
-        }
-
+        
         public FlightControls()
         {
             InitializeComponent();
             joystickVM = new JoystickViewModel(new PlaneModel(new ModelTelnetClient()));
             DataContext = joystickVM;
         }
-
-        private void ThrottleSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { FC_Throttle = e.NewValue; }
-
-        private void AileronSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) { FC_Aileron = e.NewValue; }
     }
 }
