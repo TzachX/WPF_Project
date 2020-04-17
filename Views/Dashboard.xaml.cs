ef7a4f47ef6e2b7ff75b0602e6439a0499adbf5f
NotAHoe <57091457+NotAHoe@users.noreply.github.com>
2020-04-17 05:57:59 +0300

    Update Dashboard.xaml.cs

--- Views/Dashboard.xaml.cs
+++ Views/Dashboard.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using Ex1.Model;
+using Ex1.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,76 +22,17 @@ namespace Ex1.Views
     /// </summary>
     public partial class Dashboard : UserControl
     {
-        // Properties definition
-        public static readonly DependencyProperty headingDeg = DependencyProperty.Register("HeadingDegree", typeof(double), typeof(Dashboard), null);
-        public static readonly DependencyProperty verSpeed = DependencyProperty.Register("VerticalSpeed", typeof(double), typeof(Dashboard), null);
-        public static readonly DependencyProperty groundSpeed = DependencyProperty.Register("GroundSpeed", typeof(double), typeof(Dashboard), null);
-        public static readonly DependencyProperty speed = DependencyProperty.Register("Speed", typeof(double), typeof(Dashboard), null);
-        public static readonly DependencyProperty altitude = DependencyProperty.Register("Altitude", typeof(double), typeof(Dashboard), null);
-        public static readonly DependencyProperty rollDeg = DependencyProperty.Register("RollDegree", typeof(double), typeof(Dashboard), null);
-        public static readonly DependencyProperty pitchDeg = DependencyProperty.Register("PitchDegree", typeof(double), typeof(Dashboard), null);
-        public static readonly DependencyProperty altimAltitude = DependencyProperty.Register("AltimAltitude", typeof(double), typeof(Dashboard), null);
+      
+         DashBoardViewModel vm;
 
-        // Getter + Setter for the headingDeg dependency property
-        public double HeadingDegree
-        {
-            get { return Convert.ToDouble(GetValue(headingDeg)); }
-            set { SetValue(headingDeg, value); }
-        }
-
-        // Getter + Setter for the verSpeed dependency property
-        public double VerticalSpeed
-        {
-            get { return Convert.ToDouble(GetValue(verSpeed)); }
-            set { SetValue(verSpeed, value); }
-        }
-
-        // Getter + Setter for the groundSpeed dependency property
-        public double GroundSpeed
-        {
-            get { return Convert.ToDouble(GetValue(groundSpeed)); }
-            set { SetValue(groundSpeed, value); }
-        }
-
-        // Getter + Setter for the speed dependency property
-        public double Speed
-        {
-            get { return Convert.ToDouble(GetValue(speed)); }
-            set { SetValue(speed, value); }
-        }
-
-        // Getter + Setter for the altitude dependency property
-        public double Altitude
-        {
-            get { return Convert.ToDouble(GetValue(altitude)); }
-            set { SetValue(altitude, value); }
-        }
-
-        // Getter + Setter for the rollDeg dependency property
-        public double RollDegree
-        {
-            get { return Convert.ToDouble(GetValue(rollDeg)); }
-            set { SetValue(rollDeg, value); }
-        }
-
-        // Getter + Setter for the pitchDeg dependency property
-        public double PitchDegree
-        {
-            get { return Convert.ToDouble(GetValue(pitchDeg)); }
-            set { SetValue(pitchDeg, value); }
-        }
-
-        // Getter + Setter for the altimAltitude dependency property
-        public double AltimAltitude
-        {
-            get { return Convert.ToDouble(GetValue(altimAltitude)); }
-            set { SetValue(altimAltitude, value); }
-        }
-
-        // C-tor
         public Dashboard()
         {
+
             InitializeComponent();
+            vm = new DashBoardViewModel(new PlaneModel(new ModelTelnetClient()));
+            DataContext = vm;
         }
+
+    
     }
 }
