cb57e8d96d523f2abcbfe17c15179685a757cc66
NotAHoe <57091457+NotAHoe@users.noreply.github.com>
2020-04-18 04:35:44 +0300

    Add files via upload

---
+++ ViewModels/ConnectionViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Ex1.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1.ViewModels
+{
+    public class ConnectionViewModel: INotifyPropertyChanged
+    {
+        private IPlaneModel planeModel;
+        public ConnectionViewModel(IPlaneModel model)
+        {
+            this.planeModel = model;
+            model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyProperyChange("VM_" + e.PropertyName);
+            };
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyProperyChange(string propName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+   
+        public void disconnect()
+        {
+            this.planeModel.disconnect();
+        }
+
+
+        public async void Connect(string ip, int port)
+        {
+            _ = await planeModel.connect(ip, port);
+            if (planeModel.checkConnection)
+            {
+                planeModel.start();
+
+            }
+
+
+        }
+    
+    }
+}
